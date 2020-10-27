56212d9233b466a224b3de0f18f22d1ab4a488f8
danbi huh <danbihuh00@gmail.com>
2020-10-27 19:10:37 +0900

    2020.10.27) data update

--- portfolio/js/portfolioVue.js
+++ portfolio/js/portfolioVue.js
@@ -67,6 +67,12 @@ const career = new Vue({
   el: '.career_cont',
   data:{
     list: [{ 
+      tit: '[노벨알엔디] 삼성전자 DS 프로젝트 (대외비)', 
+      desc : '프리랜서',
+      date : '2019.09.16 ~ 2020.06.30',
+      use : ['html','css','javascript'],
+      txt : '(대외비) HTML5와 CSS3를 기본으로 UI에 필요한 프론트 개발을 함께 진행하였습니다.'
+    },{ 
       tit: 'SK C&C AI 프로젝트', 
       desc : '프리랜서',
       date : '2019.09.16 ~ 2020.06.30',
